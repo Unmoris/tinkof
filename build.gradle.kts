a718a746da8649a0f631ff07a00e890090a1dbe2
micla <adraydante@gmail.com>
2022-04-06 12:51:38 +0300

    create repository

---
+++ build.gradle.kts
@@ -0,0 +1,42 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.6.5"
+	id("io.spring.dependency-management") version "1.0.11.RELEASE"
+	kotlin("jvm") version "1.6.10"
+	kotlin("plugin.spring") version "1.6.10"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+	implementation("org.springdoc:springdoc-openapi-ui:1.6.6")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+
+	testImplementation("com.ninja-squad:springmockk:3.1.1")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("io.kotest.extensions:kotest-extensions-spring:1.1.0")
+	testImplementation("io.kotest:kotest-runner-junit5-jvm:5.1.0")
+	testImplementation("io.mockk:mockk:1.12.3")
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "11"
+	}
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
---
+++ src/main/kotlin/com/example/spring_homework/SpringHomeworkApplication.kt
@@ -0,0 +1,11 @@
+package com.example.spring_homework
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringHomeworkApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringHomeworkApplication>(*args)
+}
